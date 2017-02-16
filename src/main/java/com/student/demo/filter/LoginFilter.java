9866c6589b0da0f480ff085b6e2bd1ef2c06f96f
choang91 <choang@wisc.edu>
2017-02-15 23:56:11 -0800

    Add Filter

---
+++ src/main/java/com/student/demo/filter/LoginFilter.java
@@ -0,0 +1,50 @@
+package com.student.demo.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter("/*")
+public class LoginFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		System.out.println("init");
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+		HttpSession session = request.getSession(false);
+		String loginURI = request.getContextPath() + "/login.com";
+		String loginPostURI = request.getContextPath() + "/dologin.com";
+
+		boolean loggedIn = session != null && session.getAttribute("name") != null;
+		boolean loginRequest = request.getRequestURI().equals(loginURI);
+		boolean loginPostRequest = request.getRequestURI().equals(loginPostURI);
+
+		if (loggedIn || loginRequest || loginPostRequest) {
+			chain.doFilter(request, response);
+		} else {
+			response.sendRedirect(loginURI);
+		}
+
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("destroy");
+	}
+
+}
